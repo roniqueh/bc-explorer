982e358ed362d77494dc588b8e3fc087990a9e0c
roniqueh <roniquehossain@gmail.com>
2023-02-15 04:36:24 +0000

    v5 - fix subscriber exclusives appearing in results

--- st_bc_explorer.py
+++ st_bc_explorer.py
@@ -15,7 +15,6 @@ st.set_page_config(
 
 st.caption('[contact for bugs/suggestions :)](https://instagram.com/rxniqueh)')
 st.title('BANDCAMP EXPLORER')
-st.write('testtest')
 hide_streamlit_style = """
                 <style>
                 @import url('https://fonts.googleapis.com/css2?family=Syne:wght@400;700&display=swap');
